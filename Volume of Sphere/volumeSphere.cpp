3e609abec3a6d512ae49f143ca6e79e28aedc5cd
Kaide <kaidewu@protonmail.com>
2022-03-18 00:09:00 +0100

    Upload volume of sphere exercise

---
+++ Volume of Sphere/volumeSphere.cpp
@@ -0,0 +1,20 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+double volumeSphere(double radius){
+    double volume;
+    volume = 4/3 * M_PI * pow(radius, 3);
+    return volume;
+}
+
+int main(){
+    double r;
+    double result;
+    cout << "Input the radius of the sphere: ";
+    cin >> r;
+    result = volumeSphere(r);
+    cout << "The volume of the sphere is: " << result;
+}
